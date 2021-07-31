fabc06110a0beecad4be33f2ed63c1f516acbc01
Me <me@Mes-MacBook-Pro.Dlink>
2021-07-31 16:19:37 +0530

    aa

--- src/components/grid.vue
+++ src/components/grid.vue
@@ -48,6 +48,7 @@ export default class HelloWorld extends Vue {
 
   public selectionStarted():void{
     this.fields.forEach(row => row.forEach(field =>field.selected = false));
+    ///some test
     this.isMouseDown = true;
   }
 }
